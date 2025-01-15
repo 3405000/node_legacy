61bcfe9adcc190f78af361a7ce6da8d4e08ce408
3405000 <jaeeunnooh@gmail.com>
2025-01-15 15:33:01 +0900

    refactor: Ajax -> Fetch API로 변경

--- app.js
+++ app.js
@@ -54,6 +54,42 @@ app.get('/contact', (req, res) => {
     res.render('contact');
 })
 
+app.post('/api/contact', (req, res) => {
+  const name = req.body.name;
+  const phone = req.body.phone;
+  const email = req.body.email;
+  const memo = req.body.memo;
+
+  const SQL_Query = `INSERT INTO contact(name, phone, email, memo, create_at, modify_at)
+                  VALUES ('${name}', '${phone}', '${email}', '${memo}', NOW(), NOW())`
+  
+  connectionPool.query(SQL_Query, (err, result) => {
+      if (err) {
+          console.error('데이터 삽입 중 에러 발생: ', err);
+          res.status(500).send('내부 서버 오류')
+      } else {
+          console.log('데이터가 삽입 되었습니다.');
+          res.send("<script> alert('문의사항이 등록되었습니다.'); location.href='/' </script>")
+      }
+  })
+})
+
+app.get('/contactList', (req, res) => { 
+  const selectQuery = `SELECT * FROM contact ORDER BY id DESC`;
+
+  connectionPool.query(selectQuery, (err, result) => {
+      if (err) {
+          console.error('데이터 조회 중 에러 발생: ', err);
+          res.status(500).send('내부 서버 오류')
+      } else {
+          console.log('데이터가 조회 되었습니다.');
+          console.log(result);
+          res.render('contactList', {lists:result});
+      }
+  });
+});
+
+
 app.post('/api/contactUpdate/:id', (req, res) => {
     const id = req.params.id;
     const status = "done";
@@ -122,6 +158,36 @@ app.post('/api/contactUpdate/:id', (req, res) => {
   const updateQuery = `UPDATE contact SET status = '${status}' WHERE id = '${id}';`;
 
   connection.query(updateQuery, (err, result) => {
+      if (err) {
+          console.error('데이터 업데이트 중 에러 발생:', err);
+          res.status(500).send('내부 서버 오류');
+      } else {
+          console.log('데이터가 업데이트되었습니다.');
+          res.send("<script>alert('문의사항이 업데이트되었습니다.'); location.href='/contactList'</script>");
+      }
+  });
+});
+
+app.delete('/api/contactDelete/:id', (req, res) => {
+  const id = req.params.id;
+  const deleteQuery = `delete from contact where id='${id}'`;
+  connectionPool.query(deleteQuery, (err, result) => {
+      if (err) {
+          console.error('데이터 삭제 중 에러 발생:', err);
+          res.status(500).send('내부 서버 오류');
+      } else {
+          console.log('데이터가 삭제되었습니다.');
+          res.send("<script>alert('문의사항이 삭제되었습니다.'); location.href='/contactList'</script>");
+      }
+  });
+});
+
+app.put('/api/contactUpdate/:id', (req, res) => {
+  const id = req.params.id;
+  const status = "done";
+  const updateQuery = `UPDATE contact SET status = '${status}' WHERE id = '${id}';`;
+
+  connectionPool.query(updateQuery, (err, result) => {
       if (err) {
           console.error('데이터 업데이트 중 에러 발생:', err);
           res.status(500).send('내부 서버 오류');
